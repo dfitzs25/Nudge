9137bb29297dbe7189fb4a8a36ee6d8151b37582
dfitzs25 <daltonfitzs25@gmail.com>
2026-05-03 20:42:45 -0400

    Inital iOS commit

---
+++ Nudge/Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Dalton Fitzsimmons on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
